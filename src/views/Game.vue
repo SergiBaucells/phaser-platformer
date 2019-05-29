be355925db621fbfd94aeca959819bf5fc6a35c9
SergiBaucells <sergibaucells@iesebre.com>
2019-05-29 20:03:45 +0200

    Acabat!

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,125 +1,182 @@
 <template>
-    <div id="game"></div>
+  <div id="game"></div>
 </template>
-
 <script>
-import Phaser from 'phaser'
-import wall from '../assets/wall.png'
-import ground from '../assets/ground.png'
-import player from '../assets/player.png'
-import coin from '../assets/coin.png'
-import enemy from '../assets/enemy.png'
+/* eslint-disable */
+import Phaser from "phaser";
+import wall from "../assets/wall.png";
+import ground from "../assets/ground.png";
+import player from "../assets/player.png";
+import coin from "../assets/coin.png";
+import enemy from "../assets/enemy.png";
+import die_sound from "../assets/dead.mp3";
+import takecoin_sound from "../assets/coin.mp3";
+import explosion from "../assets/explosion.png";
+let score = 0;
+let scoreText;
+let lives = 3;
+let livesText;
 
-let score = 0
-let scoreText
-
-// TODO: Detectar quan l'usuari surt del mon i executar un die
-
-function takeCoin (player, coin) {
-  // TODO: Executar el so que toca (coin.mp3) + Actualitzar un score de punts
-  coin.disableBody(true, true)
-  score = score + 10
-  // this.add.text(10, 10, 'Score: ' + score, { fontSize: '12px', fill: '#000' })
-  scoreText.setText('Score: ' + score)
-}
-
-function die (player, enemy) {
-  // TODO: Executar el so que toca (coin.mp3)
-  // TODO: Actualitzar un score de punts
-  // TODO: Shake
+function die(player, enemy) {
+  this.explosion = this.physics.add.sprite(
+    player.body.x + player.body.height / 2,
+    player.body.y + player.body.width / 2,
+    "explosion"
+  );
+  this.explosion.anims.play("explosion", false);
   player.scene.cameras.main.shake(500)
-  // TODO: Animacions
-  player.disableBody(true, true)
+  player.disableBody(true, true);
+  setTimeout(() => {
+    this.sound.play("die");
+    this.cameras.main.shake(500);
+    this.scene.restart();
+    lives = lives - 1;
+    livesText.setText("Lives: " + lives);
+    if (lives == 0) {
+      this.scene.stop();
+    }
+  }, 50);
 }
 
+function onWorldBoundsCollide() {
+  this.explosion = this.physics.add.sprite(
+    this.player.body.x + this.player.body.height / 2,
+    this.player.body.y + this.player.body.width / 2,
+    "explosion"
+  );
+  this.explosion.anims.play("explosion", false);
+  this.player.disableBody(true, true);
+  setTimeout(() => {
+    this.sound.play("die");
+    this.cameras.main.shake(500);
+    this.scene.restart();
+    lives = lives - 1;
+    livesText.setText("Lives: " + lives);
+    if (lives == 0) {
+      this.scene.stop();
+    }
+  }, 50);
+}
+
+function takeCoin(player, coin) {
+  coin.body.enable = false
+  this.tweens.add({
+    targets: coin,
+    props: {
+      y: { value: -800, duration: 500, ease: "Linear" }
+    }
+  });
+  setTimeout(() => {
+    coin.disableBody(true, true);
+  }, 2000);
+  this.sound.play("take_coin");
+  score = score + 10;
+  scoreText.setText("Score: " + score);
+}
 export default {
-  name: 'Game',
-  mounted () {
-    // Phase 3.0 -> phaser
+  name: "Game",
+  mounted() {
+    // PHASER 2.6 -> phaser-ce
+    // PHASER 3.0 -> phaser
     let config = {
       type: Phaser.AUTO,
       width: 500,
       height: 200,
       physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
           gravity: { y: 200 }
         }
       },
-      // No hi ha states a 3.0, hi ha scenes (pantalles)
+      // NO HI HA STATES A 3.0 -> SCENES
       scene: {
-        preload () {
-          // Carregar assets -> imatges, players, audios, videos etc.
-
-          // Imatges
-          this.load.image('wall', wall)
-          this.load.image('ground', ground)
-          this.load.image('coin', coin)
-          this.load.image('enemy', enemy)
-          this.load.spritesheet('player', player, { frameWidth: 28, frameHeight: 22 })
-
-          // Audio
-          // this.load.setBaseUrl('http://labs.phaser.io')
-          // TODO: Mirar si funciona el audio
-          // this.load.audio('dust', ['../assets/dust.wav', '../assets/dust.mp3'])
+        preload() {
+          this.load.spritesheet("explosion", explosion, {
+            frameWidth: 48,
+            frameHeight: 48
+          });
+          // CARREGAR ASSETS -> IMAGES, PLAYERS (SPRITESHEETS), AUDIOS, VIDEOS
+          this.load.image("wall", wall);
+          this.load.image("ground", ground);
+          this.load.spritesheet("player", player, {
+            frameWidth: 28,
+            frameHeight: 22
+          });
+          this.load.image("coin", coin);
+          this.load.image("enemy", enemy);
+          // AUDIO
+          this.load.audio("die", die_sound);
+          this.load.audio("take_coin", takecoin_sound);
         },
-        create () {
-          // Inicialitzador del nivell
-
-          // Fons
-          this.cameras.main.backgroundColor.setTo(52, 152, 219)
-
-          // Grup per defecte es dinàmic
-          // this.level = this.physics.add.group()
-          this.level = this.physics.add.staticGroup()
-
-          // Mon
-          // this.add.image(500 / 2 - 160, 200 / 2, 'wall')
-          // this.add.image(500 / 2 + 160, 200 / 2, 'wall')
-          // this.ground = this.physics.add.image(500 / 2, 200 / 2 + 30, 'ground')
-          this.level.create(500 / 2 - 160, 200 / 2, 'wall')
-          this.level.create(500 / 2 + 160, 200 / 2, 'wall')
-          this.level.create(500 / 2, 200 / 2 + 30, 'ground')
-
-          // this.ground.body.allowGravity = false
-
-          // Player
-          this.player = this.physics.add.sprite(500 / 2, 200 / 2 - 50, 'player')
-          // this.player.setCollideWorldBounds(true)
-          this.player.setBounce(0.2)
-          this.physics.add.collider(this.player, this.level)
-
-          // Define sounds
-          // this.dustSound = this.add.audio('dust', 0.1)
-
-          // Cursors
-          this.cursors = this.input.keyboard.createCursorKeys()
-
-          // Animate player
+        create() {
+          // INITILIZE del nivell -> Afegirem tiles (level: pareds, terras), afegirem player/s, collectibles (coins) , enemies
+          this.sound.add("die");
+          this.sound.add("take_coin");
+          this.cameras.main.backgroundColor.setTo(52, 152, 219);
+          // GROUP PER DEFECTE ES DINAMIC
+          this.level = this.physics.add.staticGroup();
+          this.level.create(500 / 2 - 160, 200 / 2, "wall");
+          this.level.create(500 / 2 + 160, 200 / 2, "wall");
+          this.level.create(500 / 2, 200 / 2 + 30, "ground");
+          // PLAYER -> FÍSIQUES
+          this.player = this.physics.add.sprite(
+            500 / 2,
+            200 / 2 - 50,
+            "player"
+          );
+          this.player.setBounce(0.2);
+          this.player.body.setCollideWorldBounds(true);
+          this.player.body.onWorldBounds = true;
+          this.physics.add.collider(this.player, this.level);
+          // cursors
+          this.cursors = this.input.keyboard.createCursorKeys();
+          // ANIMATE PLAYER
           this.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("player", {
+              start: 3,
+              end: 5
+            }),
             frameRate: 5,
             repeat: -1
-          })
-
-          // Add coins
-          this.coins = this.physics.add.group()
-          this.coins.create(140, 200 / 2, 'coin')
-          this.coins.create(170, 200 / 2, 'coin')
-          this.coins.create(200, 200 / 2, 'coin')
-          this.physics.add.collider(this.coins, this.level)
-          this.physics.add.overlap(this.player, this.coins, takeCoin, null, this)
-
-          // Score
-          scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: '12px', fill: '#000' })
-
-          // Enemy
-          this.enemies = this.physics.add.group()
-          this.enemies.create(500 / 2 + 130, 200 / 2, 'enemy')
-          this.physics.add.collider(this.enemies, this.level)
-          this.physics.add.overlap(this.player, this.enemies, die, null, this)
-
+          });
+          this.anims.create({
+            key: "explosion",
+            frames: this.anims.generateFrameNumbers("explosion", {
+              start: 0,
+              end: 6
+            }),
+            frameRate: 60,
+            repeat: -1
+          });
+          // ADD COINS
+          this.coins = this.physics.add.group();
+          this.coins.create(140, 200 / 2, "coin");
+          this.coins.create(170, 200 / 2, "coin");
+          this.coins.create(200, 200 / 2, "coin");
+          this.physics.add.collider(this.coins, this.level);
+          this.physics.add.overlap(
+            this.player,
+            this.coins,
+            takeCoin,
+            null,
+            this
+          );
+          // SCORE
+          scoreText = this.add.text(10, 10, "Score: " + score, {
+            fontSize: "12px",
+            fill: "#000"
+          });
+          livesText = this.add.text(10, 30, "Lives: " + lives, {
+            fontSize: "12px",
+            fill: "#000"
+          });
+          // ENEMIES
+          this.enemies = this.physics.add.group();
+          this.enemies.create(500 / 2 + 130, 200 / 2, "enemy");
+          this.physics.add.collider(this.enemies, this.level);
+          this.physics.add.overlap(this.player, this.enemies, die, null, this);
+          this.physics.world.on("worldbounds", onWorldBoundsCollide, this);
           //  prepare loser txt
           this.loserText = this.add.text(500 / 2, 200 / 2 - 50, 'You lose!', { fontSize: '24px', fill: '#000' }).setVisible(false)
           this.cameras.main.on('camerashakestart', () => {
@@ -129,34 +186,33 @@ export default {
             this.loserText.setVisible(false)
           })
         },
-        update () {
-          this.player.anims.play('idle', true)
-          // S'executa continuament al gameloop -> 60 vegades per segon o FPS
-
+        update() {
+          this.player.anims.play("idle", true);
+          // INPUT EVENTS
+          if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.setFrame(2);
+          } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.setFrame(1);
+          } else {
+            this.player.setVelocityX(0);
+          }
+          // JUMP
+          if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-160);
+          }
+          // console.log('UPDATED')
+          // ESTE EL QUE S'executa continuament al Game loop -> 60 vegades per segon o FPS
           // if (this.player.body.touching.down && this.player.y > 10) {
           //   this.dustSound.play()
           // }
-
-          // Input Events
-          if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160)
-            this.player.setFrame(1)
-          } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160)
-            this.player.setFrame(2)
-          } else {
-            this.player.setVelocityX(0)
-          }
-
-          // Jump
-          if (this.cursors.up.isDown && this.player.body.touching.down) {
-            this.player.setVelocityY(-160)
-          }
         }
       }
-    }
+    };
     // eslint-disable-next-line
-      new Phaser.Game(config)
+    new Phaser.Game(config);
   }
-}
+};
+
 </script>
